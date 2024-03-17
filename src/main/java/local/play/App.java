fbb2889ff75cfd2ccc3ac8d595bff5211ab1469f
RJ2523 <rajneesh.sahay79@gmail.com>
2024-03-17 23:54:45 +0530

    Implements Singleton Design Pattern

--- src/main/java/local/play/App.java
+++ src/main/java/local/play/App.java
@@ -4,25 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import local.play.prototype.ConcretePrototype;
+import local.play.singleton.Singleton;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args ){
-        ConcretePrototype concretePrototype = new ConcretePrototype();
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        concretePrototype.setId(1);
-        concretePrototype.setArr(list);
 
-        ConcretePrototype con = (ConcretePrototype) concretePrototype.clone();
-        System.out.println("Changing Array list value in clone object");
-        con.getArr().set(0, 6);
+        Singleton singleton = Singleton.getInstane();
+        System.out.println(singleton.getX());
+        Singleton singleton2 = Singleton.getInstane();
+        System.out.println(singleton2.getX());
 
-        System.out.println("Checking if the original object is intact : "  +concretePrototype.getArr());
     }
 }
---
+++ src/main/java/local/play/singleton/Singleton.java
@@ -0,0 +1,24 @@
+package local.play.singleton;
+
+import lombok.Data;
+
+@Data
+public class Singleton {
+    
+    // will not have getters and setters for this field for various reasons
+    // to maintain global state
+    private static Singleton singleton = null;
+    
+    private int x = 2;
+
+    private Singleton(){
+    }
+
+    public static Singleton getInstane(){
+        if(singleton==null){
+            System.out.println("Singleton Object created");
+            singleton = new Singleton();
+        }
+        return singleton;
+    }
+}
